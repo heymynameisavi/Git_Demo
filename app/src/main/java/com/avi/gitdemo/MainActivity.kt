b9f988b65d2568355599e511bb980929f9047386
heymynameisavi <abhilashjha264@gmail.com>
2021-09-24 05:27:42 +0530

    This is our forth commit

--- app/src/main/java/com/avi/gitdemo/MainActivity.kt
+++ app/src/main/java/com/avi/gitdemo/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is content in my second commit")
-        today is a good day
+        hdsfhsiu dufsdusu
     }
 }
